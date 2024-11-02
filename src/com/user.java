e72955905cc7ae31369a29c2f688b163a0dec3f4
Jorge Sandoval <jasandoval6@miners.utep.edu>
2024-11-02 13:53:39 -0600

    UPDATED: user class/setters&getters

--- src/com/user.java
+++ src/com/user.java
@@ -1,27 +1,100 @@
 package com;
 
-public class user{
-    private int userID; // userID.
-    private String firstName; // user first name.
-    private String middleName; // user middle name.
-    private String lastName; // user last name.
-    private String fullName = firstName + middleName + lastName; // user full name.
-    private String emailAddress; 
+public class user {
+    // Variables:
+    private int userID;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String emailAddress;
     private String eventsCreated;
     private String eventsAttended;
+
+    // Constructor
+    public user(int userID, String firstName, String middleName, String lastName, String emailAddress) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+    }
+
+    // Getters and Setters:
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getEventsCreated() {
+        return eventsCreated;
+    }
+
+    public void setEventsCreated(String eventsCreated) {
+        this.eventsCreated = eventsCreated;
+    }
+
+    public String getEventsAttended() {
+        return eventsAttended;
+    }
+
+    public void setEventsAttended(String eventsAttended) {
+        this.eventsAttended = eventsAttended;
+    }
+
+    // Method to return the full name
+    public String getFullName() {
+        // Handles cases where middleName might be null
+        if (middleName == null || middleName.isEmpty()) {
+            return firstName + " " + lastName;
+        } else {
+            return firstName + " " + middleName + " " + lastName;
+        }
+    }
+
+    // toString method for easy display of user information
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", fullName='" + getFullName() + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", eventsCreated='" + eventsCreated + '\'' +
+                ", eventsAttended='" + eventsAttended + '\'' +
+                '}';
+    }
 }
-
-/*
- * Variables: 
-•	userId: Unique ID for the user. 
-•	name: User’s full name. 
-•	email: User’s email address. 
-•	eventsCreated: List of event IDs the user has created. 
-•	eventsAttended: List of event IDs the user has attended. 
-
-Methods: 
-•	create_event(event): Allows the user to create an event. 
-•	attend_event(event): Adds the event to the user’s attended events list. 
-•	cancel_attendance(event): Removes the event from the user’s attended events list. 
- * 
- */
