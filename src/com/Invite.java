d0582598f0d788a7e9f9459c23f84a979f2370f0
salazaritzel <salazitzel@gmail.com>
2024-11-05 03:27:21 -0700

    Updated invite class

--- src/com/Invite.java
+++ src/com/Invite.java
@@ -1,56 +1,102 @@
-import java.util.*;
+package com;
+import java.util.UUID;
+import java.util.ArrayList;
 
 /**
  * Invite class responsible for handling the invite system. This module sends real-time 
  * updates to users, including RSVP confirmations (rsvp) and invitation to events.
- * THATS ALL IT DOES. WE DONT MANAGE THE WAITLIST THATS @RSVPMANAGER job. WE JUST SEND THE INVITE.
+ * THATS ALL IT DOES. WE DON'T MANAGE THE GUEST LIST THATS @RSVPMANAGER job. WE JUST SEND THE INVITE.
  */
 public class Invite {
-    private String inviteID;
+    private user guest;
     private String eventID;
-    private EventPlanner event; // ^^^ From Event class, should eventID come from event class? event.getEventID
+    private String inviteID;
+    private EventPlanner events;   // From Event class, should eventID come from event class? event.getEventID
+    private String invitedUserID; // Does this mean every user gets an ID so we don't go by name. Do we randomize it?
+    private inviteStatus status;
 
-    private String invitedUserID; // Does this mean every user gets an ID so we don't recognize by name. Do we randomize it?
-                                  // does this mean this class has to be connected to the wedding and
-    private boolean inviteStatus; // PENDING or CONFIRMED
+    // We will be using enumerated values as these valued will remain constant throughout the entire program
+    public enum inviteStatus {
+        PENDING, CONFIRMED, DECLINED
+    } 
 
-    public Invite(String inviteID, String eventID, String invitedUserID, boolean inviteStatus) {
-        this.inviteID = inviteID;
-        this.eventID = eventID; // = Event.eventId();
-        this.inviteStatus = inviteStatus;
-
-        // sendInvite(eventId, inviteeList): This is from the User class. Sends invitations to invitees. 
+    public Invite(user guest, String eventID, String inviteID, EventPlanner event, String invitedUserID, inviteStatus status) {
+        this.guest = guest;
+        this.eventID = UUID.randomUUID().toString().substring(0, 8); // EventPlanner.getEventId(); EventPlanner should have a getEventID class
+        this.inviteID = setInviteID();
+        this.events = event;
+        this.invitedUserID = setInvitedUserID();
+        this.status = inviteStatus.PENDING;
     }
 
+    public Invite(user guest, String eventID) {
+        this.guest = guest;
+        this.eventID = UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public boolean checkStatus(String stat) {
+        return status.toString().equalsIgnoreCase(stat);
+    }   
+
+    /*
+     * SETTERS
+     */
+    public String setInviteID() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public String setInvitedUserID() {
+        return UUID.randomUUID().toString().substring(0, 10);
+    }
+
+    public void setStatus(inviteStatus status) {
+        this.status = status;
+    }
+
+    /*
+     * GETTERS
+    */
     public String getInviteID() {
         return inviteID;
     }
 
     public String getEventID() {
-        return inviteID;
+        return eventID;
     }
 
     public String getInvitedUserID() {
         return invitedUserID;
     }
 
-    public boolean getInviteStatus() {
-        return inviteStatus;
+    public inviteStatus getInviteStatus() {
+        return status;
     }
 
-    public String generateInvitedUserID() { // this as an attribute instead?
-        return invitedUserID = UUID.randomUUID().toString().substring(0, 10);
-    }
-
-    public boolean sendInvite(String eventID, String invitedUserID) { // or 
-
+    public boolean sendInvite(String eventID, String invitedUserID) {
+        EventPlanner event = getEventID(eventID);
+        Invite newInvite = new Invite(guest, eventID, status.PENDING)
+        
     }
 
     public void inviteResponse() {
+
+        if(getInviteStatus().equalsIgnoreCase("CONFIRMED")) {
+            ArrayList<String> confirmed = new ArrayList<String>();
+            confirmed.add(invitedUserID);
+
+        }
         // if user selects confirm
         // accept = true;
         // add invitedUserID to rsvp manager list
         // else, accepts = false;
 
+    }
+
+    // Sample usage
+    public static void main(String[] args) {
+
+        // we need an email attr to send it to the user
+        Invite newInvite = new Invite();
+        
     }
 }
